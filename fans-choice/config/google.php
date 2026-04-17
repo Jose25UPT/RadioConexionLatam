e8fd662a227799f4a21d6a645c15a0ae94736ca1
GustavoValleBustamante <gusvalle13@gmail.com>
2026-04-17 00:47:34 -0500

    fans-choice v2.4

--- fans-choice/config/google.php
+++ fans-choice/config/google.php
@@ -7,10 +7,7 @@ $google_client = new Google_Client();
 $google_client->setClientId(getenv('FC_GOOGLE_CLIENT_ID'));
 $google_client->setClientSecret(getenv('FC_GOOGLE_CLIENT_SECRET'));
 
-// Redirect URI dinámica: funciona en local y en producción automáticamente
-$protocol     = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https' : 'http';
-$host         = $_SERVER['HTTP_HOST'] ?? 'localhost';
-$redirect_uri = $protocol . '://' . $host . APP_BASE . '/auth.php';
+$redirect_uri = getenv('FC_GOOGLE_REDIRECT_URI') ?: APP_BASE . '/auth.php';
 $google_client->setRedirectUri($redirect_uri);
 
 $google_client->addScope('email');
