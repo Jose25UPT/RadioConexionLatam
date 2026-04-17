71eb2c1b2b07a7dfc749f14161d1a8604ce61d77
GustavoValleBustamante <gusvalle13@gmail.com>
2026-04-17 00:49:32 -0500

    fans-choice v2.4.1

--- fans-choice/auth.php
+++ fans-choice/auth.php
@@ -1,6 +1,19 @@
 <?php
+ob_start();
+
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
+ini_set('display_errors', 0);
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/logs/error.log');
+
+require_once __DIR__ . '/vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
+
 require_once __DIR__ . '/config/app.php';
 require_once __DIR__ . '/controllers/AuthController.php';
 
 $authController = new AuthController();
 $authController->login();
+
+ob_end_flush();
